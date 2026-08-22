7f0c6408e2874e64085eea178ffa16091eab2073
Alex <107483671+AlexGallego2005@users.noreply.github.com>
2026-08-22 10:53:53 +0200

    Nuevos + migración
    Algunos docs nuevos y bastantes migrados de WikiJS a aquí

---
+++ astro.config.mjs
@@ -0,0 +1,123 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
+import starlightLinksValidator from 'starlight-links-validator';
+import starlightCoolerCredit from 'starlight-cooler-credit';
+import starlightAutoSidebar from 'starlight-auto-sidebar';
+import starlightSidebarTopics from 'starlight-sidebar-topics';
+import starlightAutoDrafts from 'starlight-auto-drafts';
+import starlightKbd from 'starlight-kbd';
+import starlightAnnouncement from 'starlight-announcement';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Fansub Hub',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/fansubhub/wiki' },
+				{ icon: 'discord', label: '¡Únete a nuestro servidor!', href: 'https://discord.gg/rt3zuNGyA9' }
+			],
+			plugins: [
+				starlightThemeRapide(),
+				starlightLinksValidator(),
+				starlightCoolerCredit({
+					credit: {
+						title: "¿Tienes dudas o sugerencias?",
+						description: "¡Entra a nuestro servidor de Discord y coméntanoslas!",
+						href: 'https://discord.gg/rt3zuNGyA9'
+					}
+				}),
+				starlightAutoSidebar(),
+				starlightSidebarTopics([
+					{
+						label: 'Aegisub',
+						link: '/aegisub/',
+						icon: 'pencil',
+						items: [
+							{ 
+								label: 'Introducción',
+								items: ['aegisub']
+							},
+							{
+								label: 'Plugins',
+								items: [{ autogenerate: { directory: 'aegisub/plugins' } }],
+							},
+							{
+								label: 'Etiquetas',
+								items: [{ autogenerate: { directory: 'aegisub/tags' } }],
+							}
+						]
+					},
+					{
+						label: 'Tipos de archivos',
+						link: '/files/',
+						icon: 'document',
+						items: [
+							{ 
+								label: 'Introducción',
+								items: ['files']
+							},
+							{ 
+								label: 'Tipos de archivos',
+								items: [{ autogenerate: { directory: 'files' } }],
+							}
+						]
+					},
+					{
+						label: 'Repositorio de guías',
+						link: '/guides/',
+						icon: 'open-book',
+						items: [
+							{ 
+								label: 'Scripts y plugins',
+								items: [
+									{ autogenerate: { directory: 'guides/scripts/' } },
+									{
+										label: "Aegisub",
+										items: [{ autogenerate: { directory: 'aegisub/plugins/thirdparty/' } }]
+									}
+								]
+							},
+							{ 
+								label: 'Varios',
+								items: [
+									'guides/mkv/multiplex',
+									'guides/mkv/muxing-batch-gui'
+								]
+							}
+						]
+					}
+				]),
+				starlightAutoDrafts(),
+				starlightKbd({
+					globalPicker: false,
+					types: [
+						{ id: 'windows', label: 'Windows', default: true }
+					]
+				}),
+				starlightAnnouncement({
+					announcements: [
+						{
+							id: 'update-pending',
+							content: 'Esta página está pendiente de recibir una actualización.',
+							variant: 'caution',
+							showOn: [
+								'/aegisub/tags/advanced/transform',
+								'/aegisub/plugins/dependency_control',
+								'/aegisub'
+							]
+						}
+					]
+				})
+			],
+			components: {
+				Sidebar: './src/components/Sidebar.astro'
+			},
+			customCss: [
+				'./src/styles/custom.css'
+			]
+		}),
+	],
+});
